6715fe6a8d31d3e1cb945fed7775fc537af5874d
Fernando Gabriel M. da Silva <fernando.silva@ccc.ufcg.edu.br>
2020-04-16 17:44:16 -0300

    importanto e testando o react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
-
+import { BrowserRouter, Route, Redirect, Link, Switch } from 'react-router-dom';
 
 function App() {
-    return (<h1>Hello world</h1>);
+    return (
+        <>
+            <BrowserRouter>
+
+                <header>
+                    <Link to="/">HOME |</Link>
+                    <Link to="/page"> Page</Link>
+                </header>
+                <main>
+                    <Switch>
+                        <Route exact path="/" component={() => <h1>Hello World</h1>} />
+                        <Route path="/page" component={() => <h1>Page 2</h1>} />
+                    </Switch>
+                </main>
+
+                <footer>
+
+                </footer>
+            </BrowserRouter>
+
+        </>
+    );
 }
 
 export default App;
