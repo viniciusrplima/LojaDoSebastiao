4acb921657d4de23e00918a4bfc7bb49aa916eba
Fernando Gabriel M. da Silva <fernando.silva@ccc.ufcg.edu.br>
2020-04-16 19:45:06 -0300

    Criação das principais rotas da aplicação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Route, Redirect, Link, Switch } from 'react-router-dom';
+import Routes from './routes';
 
 function App() {
-    return (
-        <>
-            <BrowserRouter>
-
-                <header>
-                    <Link to="/">HOME |</Link>
-                    <Link to="/page"> Page</Link>
-                </header>
-                <main>
-                    <Switch>
-                        <Route exact path="/" component={() => <h1>Hello World</h1>} />
-                        <Route path="/page" component={() => <h1>Page 2</h1>} />
-                    </Switch>
-                </main>
-
-                <footer>
-
-                </footer>
-            </BrowserRouter>
-
-        </>
-    );
+    return (<Routes/>);
 }
 
 export default App;
