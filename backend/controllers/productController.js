164b704526ef24adf8b5837ed5ac5cb5d8ee047b
Vinicius Rodrigues <vinicius.rodrigues.lima@ccc.ufcg.edu.br>
2020-04-15 22:34:08 -0300

    Adicão de comentários

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,13 +1,14 @@
 
 const Product = require('../models/Product');
 
-
+// Retorna uma lista dos produtos de determinada categoria
 const index = async (req, res) => {
     const category = req.params.category;
     const products = await Product.find({ category });
     res.send(products);
 }
 
+// Armazena um produto
 const store = async (req, res) => {
 
     const product = new Product(req.body);
@@ -22,6 +23,7 @@ const store = async (req, res) => {
 
 }
 
+// Atualiza um produto com determinado id
 const update = async (req, res) => {
     const _id = req.body._id;
     const data = req.body;
@@ -30,6 +32,7 @@ const update = async (req, res) => {
     res.send(product);
 }
 
+// Remove um produto com determinado id
 const remove = async (req, res) => {
     const _id = req.params.id;
     const product = await Product.findByIdAndDelete({ _id });
--- backend/routes.js
+++ backend/routes.js
@@ -3,9 +3,11 @@ const express = require('express');
 const router = express.Router();
 const productController = require('./controllers/productController');
 
+// Rotas para manipulação dos produtos
 router.get('/:category', productController.index);
 router.post('/', productController.store);
 router.put('/', productController.update);
 router.delete('/:id', productController.remove);
+
 
 module.exports = router;
