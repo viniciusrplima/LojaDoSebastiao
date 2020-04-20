7435cc4a04b9b7e0732f44f1955bdbd001874d26
Fernando Gabriel M. da Silva <fernando.silva@ccc.ufcg.edu.br>
2020-04-20 12:59:21 -0300

    Criando o componente Cards

--- frontend/src/components/Newproduct/Newproduct.js
+++ frontend/src/components/Newproduct/Newproduct.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 
 export default function Newproduct() {
-    return (
-        <div class="section" id="index-banner">
+  return (
+    <div class="section" id="index-banner">
           <div class="row red" style={{marginBottom: '0'}}>
             <div class="col s12 m10 offset-m2 l8 offset-l4 xl8 offset-xl3 center-on-small-only">
-              <h2 class="header">Novo Produto</h2>
+              <h2 class="header">Categoria: Newproduct</h2>
               <div className="row">
                 
               </div>
             </div>
         </div>
       </div>
-    );
-  }
+  );
+}
--- frontend/src/components/Tv/Tv.js
+++ frontend/src/components/Tv/Tv.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Cards from '../Cards/Cards';
 
 
 export default function Tv() {
@@ -8,7 +9,15 @@ export default function Tv() {
             <div class="col s12 m10 offset-m2 l8 offset-l4 xl8 offset-xl3 center-on-small-only">
               <h2 class="header">Categoria: TV's</h2>
               <div className="row">
-                
+              <div className="col s12 m6 l4 xl4">
+              <Cards></Cards>
+            </div>
+            <div className="col s12 m6 l4 xl4">
+              <Cards></Cards>
+            </div>
+            <div className="col s12 m6 l4 xl4">
+              <Cards></Cards>
+            </div>
               </div>
             </div>
         </div>
